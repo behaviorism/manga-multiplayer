d523986f9247f0937fd96226f0b558b6f54e0429
behaviorism <luka.debelic03@gmail.com>
2023-10-05 00:21:34 +0200

    fix

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    "./src/**/*.{ts,tsx}",
+    "./dist/renderer/index.html",
+    "./src/renderer/index.html",
+  ],
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
