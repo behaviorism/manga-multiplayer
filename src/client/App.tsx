0d00ee7f7098d8615ec74e142115e17d5cb41967
behaviorism <luka.debelic03@gmail.com>
2023-10-19 15:57:06 +0200

    fix

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -2,6 +2,7 @@ import { RouterProvider, createHashRouter } from "react-router-dom";
 import Link from "./Pages/Link";
 import Connect from "./Pages/Connect";
 import Host from "./Pages/Host";
+import { useEffect } from "react";
 
 const router = createHashRouter([
   {
@@ -19,6 +20,12 @@ const router = createHashRouter([
 ]);
 
 const App = () => {
+  useEffect(() => {
+    setTimeout(() => {
+      window.scrollTo(0, 1);
+    });
+  }, []);
+
   return (
     <div className="w-full h-full flex flex-col items-center bg-gray-900">
       <RouterProvider router={router} />
